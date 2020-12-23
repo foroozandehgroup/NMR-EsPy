d3715e3aac42d00c7757bc2bae1be7c091a4879a
Simon <simonhulse87@googlemail.com>
2020-12-23 09:29:32 +0000

    Renamed some stuff

--- nmrespy/__init__.py
+++ nmrespy/__init__.py
@@ -2,5 +2,5 @@ import os
 from ._version import __version__
 
 NMRESPYPATH = os.path.dirname(__file__)
-MFLOGOPATH = os.path.join(NMRESPYPATH, 'pics/mf_group_logo.pdf')
+MFLOGOPATH = os.path.join(NMRESPYPATH, 'pics/mf_logo.pdf')
 NMRESPYLOGOPATH = os.path.join(NMRESPYPATH, 'pics/nmrespy_full.png')
