f3e42821d3ed23c1177deae0393ffe6194e43193
Simon <simonhulse87@googlemail.com>
2021-02-23 14:47:49 +0000

    tweak setup.py, inc. MANIFEST.in

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('README.rst', 'r') as fh:
 exec(open('nmrespy/_version.py').read())
 
 setuptools.setup(
-    name='nmrespy',
+    name='nmrespy_testing0',
     version=__version__,
     description='NMR-EsPy: Nuclear Magnetic Resonance Estimation in Python',
     author='Simon Hulse',
     author_email='simon.hulse@chem.ox.ac.uk',
-    url='https://github.com/5hulse/NMR-EsPy',
+    url='https://github.com/foroozandehgroup/NMR-EsPy',
     long_description=long_description,
     long_description_content_type="text/x-rst",
     packages=setuptools.find_packages(),
@@ -20,5 +20,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    install_requires=[
+        "numpy>=1.20"
+        "scipy>=1.6"
+        "matplotlib>=3.3"
+        "colorama==0.4 ; platform_system == 'Windows'"
+    ],
+    python_requires='>=3.7',
 )
