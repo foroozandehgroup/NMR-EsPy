a02a81238a85c1d4bee078f4878b24554f81a49b
Simon <simonhulse87@googlemail.com>
2021-03-07 14:04:06 +0000

    Ready to work on updating GUI

---
+++ nmrespy/app/custom_widgets.py
@@ -0,0 +1,260 @@
+"""
+Customised widgets for NMR-EsPy GUI.
+"""
+
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+from .config import BGCOLOR, MAINFONT, READONLYENTRYCOLOR, BUTTONDEFAULT
+
+def generate(cls, keys, values, kwargs):
+
+    for key, value in zip(keys, values):
+        cls[key] = value
+
+    # overwrite default properties with any provided kwargs
+    for kwkey, kwvalue in zip(kwargs.keys(), kwargs.values()):
+        cls[kwkey] = kwvalue
+
+
+class MyFrame(tk.Frame):
+    """A Tkinter frame with a white background by default."""
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent)
+        generate(self, ('bg',), (BGCOLOR,), kwargs)
+
+
+class MyToplevel(tk.Toplevel):
+    """A Tkinter toplevel, which by default:
+
+    * Has a white background
+    * CAnnot be resized
+    * Has the title NMR-EsPy
+    """
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(master=parent)
+
+        generate(self, ('bg',), (BGCOLOR,), kwargs)
+
+        self.title('NMR-EsPy')
+        self.resizable(False, False)
+
+
+class MyLabel(tk.Label):
+    """Tkinter label with white background by deafult"""
+
+    def __init__(self, parent, bold=False, **kwargs):
+        super().__init__(parent)
+        keys = ('bg', 'font')
+
+        if bold:
+            values = (BGCOLOR, (MAINFONT, '11', 'bold'))
+        else:
+            values = (BGCOLOR, (MAINFONT, '11'))
+
+        generate(self, keys, values, kwargs)
+
+
+class MyButton(tk.Button):
+    """Tkinter button with various tweaks"""
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent)
+
+        keys = (
+            'width', 'highlightbackground', 'bg',
+            'disabledforeground',
+        )
+        values = (8, 'black', BUTTONDEFAULT, '#a0a0a0')
+
+        generate(self, keys, values, kwargs)
+
+        # there isnt a disabledbackground variable for tk.Button
+        # for some reason
+        # have to manually change color upon change of state
+        if self['state'] == 'disabled':
+            self['bg'] = '#e0e0e0'
+
+
+
+class MyCheckbutton(tk.Checkbutton):
+    """Tkinter button with various tweaks"""
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent)
+
+        keys = ('bg', 'highlightthickness', 'bd')
+        values = (BGCOLOR, 0, 0)
+
+        generate(self, keys, values, kwargs)
+
+
+class MyScale(tk.Scale):
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent)
+
+        keys = (
+            'orient', 'showvalue', 'sliderlength', 'bd', 'highlightthickness',
+            'highlightbackground', 'relief', 'bg', 'troughcolor',
+        )
+        values = (
+            tk.HORIZONTAL, 0, 15, 0, 1, 'black', 'flat', BGCOLOR, 'white'
+        )
+
+        generate(self, keys, values, kwargs)
+
+
+class MyEntry(tk.Entry):
+    """Entry widget with some aesthetic teweaks.
+
+    ``return_command`` specifies a callable to be bound to the widget
+    for when the user presses <Return>. If the callable has any arguments,
+    these should be provided as a tuple into ``return_args``.
+    The upshot of these commands is the text becomes red once the user
+    changes the input, and goes back to black once <Return> has been
+    pressed. The idea is to warn the user that they haven't saved their
+    changes since altering the entry widget.
+    """
+
+    def __init__(self, parent, return_command=None, return_args=None, **kwargs):
+        super().__init__(parent)
+
+        self.return_command = return_command
+        self.return_args = return_args
+
+        keys = (
+            'width', 'highlightthickness', 'highlightbackground',
+            'bg', 'readonlybackground'
+        )
+        values = (
+            7, 1, 'black', 'white', READONLYENTRYCOLOR)
+
+        generate(self, keys, values, kwargs)
+
+        if self.return_command:
+            self.bind('<Key>', lambda event: self.key_press())
+            self.bind('<Return>', lambda event: self.return_press())
+
+    def key_press(self):
+        self['fg'] = 'red'
+        self['highlightcolor'] = 'red'
+        self['highlightbackground'] = 'red'
+
+    def return_press(self):
+        self['fg'] = 'black'
+        self['highlightcolor'] = 'black'
+        self['highlightbackground'] = 'black'
+        self.return_command(*self.return_args)
+
+class MyOptionMenu(tk.OptionMenu):
+
+    def __init__(self, parent, variable, value, *values, **kwargs):
+
+        super().__init__(parent, variable, value, values, kwargs)
+
+        keys = ('bg', 'borderwidth', 'width', 'highlightcolor')
+        values = (BGCOLOR, 1, 10, 'black')
+
+        generate(self, keys, values, kwargs)
+
+        if 'bg' in kwargs.keys():
+            self['menu']['bg'] = kwayrgs['bg']
+        else:
+            self['menu']['bg'] = BGCOLOR
+
+
+class MyText(tk.Text):
+
+    def __init__(self, parent):
+
+        super().__init__(parent)
+
+        keys = ('bg', 'highlightcolor', 'highlightbackground')
+        values = (BGCOLOR, 'black', 'black')
+
+        generate(self, keys, values, kwargs)
+
+
+
+class MyNavigationToolbar(NavigationToolbar2Tk):
+    """Tweak default matplotlib navigation bar to exclude subplot-config
+    and save buttons. Also dialogues as cursor goes over plot, and bar
+    is set to be white"""
+
+    def __init__(self, canvas, parent, color=BGCOLOR):
+
+        # slice toolitems (this gets rid of the unwanted buttons)
+        self.toolitems = self.toolitems[:6]
+
+        super().__init__(canvas, parent, pack_toolbar=False)
+
+        # make everything white
+        self['bg'] = color
+        self._message_label['bg'] = color
+        for button in self.winfo_children():
+            button['bg'] = color
+
+    def set_message(self, msg):
+        pass
+
+
+class MyTable(MyFrame):
+
+    def __init__(self, parent, titles, contents, **kwargs):
+
+        super().__init__(parent, **kwargs)
+
+        self.table_frame = MyFrame(self)
+        self.table_frame.grid(column=0, row=0, padx=(0,10), pady=10)
+
+        self.title_labels = {}
+        self.labels = {}
+        self.entries = {}
+
+        rows, columns = len(contents), len(contents[0])
+
+        max_width = 0
+
+        for row in range(rows+1):
+            for column in range(columns+1):
+                if row != 0 and column == 0:
+                    # oscillator labels (1, 2, 3, etc.)
+                    self.labels[row] = MyLabel(
+                        self.table_frame, text=f"{row}", bold=True,
+                    )
+
+                    if row == rows + 1:
+                        pady = 2
+                    else:
+                        pady = (2, 0)
+
+                    self.labels[row].grid(
+                        column=column, row=row, ipadx=10, pady=pady, sticky='w',
+                    )
+
+                elif row == 0 and column != 0:
+                    self.table_frame.columnconfigure(column, weight=1)
+                    # column titles
+                    text = titles[column-1]
+                    self.title_labels[text] = MyLabel(
+                        self.table_frame, text=text, bold=True,
+                    )
+
+                    self.title_labels[text].grid(
+                        column=column, row=row, sticky='w', pady=(2,0),
+                    )
+
+                elif row != 0 and column != 0:
+                    if column == 1:
+                        self.entries[row] = {}
+
+                    self.entries[row][text] = MyEntry(
+                        self.table_frame, textvariable=contents[row-1][column-1],
+                        width=14, readonlybackground='#a0a0a0', state='readonly',
+                    )
+                    self.entries[row][text].grid(
+                        row=row, column=column, sticky='ew',
+                    )
---
+++ nmrespy/app/nmrespy_linux.py
@@ -0,0 +1,40 @@
+# nmrespy.py
+# simon.hulse@chem.ox.ac.uk
+# Jython script for Spectral Estimation of NMR data within TopSpin
+# Calls an external Python3 script for the estimation and GUI
+
+# Linux version
+
+import os
+from subprocess import *
+
+# check whether nmrest exists by importing
+# if it exists, $? = 0
+# if it does not exist, $? = int > 0
+checknmrespy = Popen(["python3 -c 'import nmrespy' ; echo $?"], shell=True, stdout=PIPE).communicate()[0]
+
+if int(checknmrespy):
+	ERRMSG('Could not find NMR-ESPY in your Python 3 PATH!', modal=1)
+	EXIT()
+
+else:
+  nmrespy_path = Popen(["python3 -c 'import nmrespy; import os; print(os.path.dirname(nmrespy.__file__))'"], shell=True, stdout=PIPE).communicate()[0]
+  nmrespy_path = nmrespy_path.rstrip()
+  print nmrespy_path
+
+# get path
+info = CURDATA()
+
+# info will be None if no active data exists. Inform user if this is the case
+if info == None:
+	ERRMSG("Please select a data set to run spectral estimation!", modal=1)
+	EXIT()
+
+data_path = '%s/%s/%s' %(info[3], info[0], info[1])
+
+proc_path = '%s/%s/%s/pdata/%s' %(info[3], info[0], info[1], info[2])
+
+with open('%s/gui/tmp/info.txt' %nmrespy_path, 'w') as fh:
+	fh.write('%s %s' %(data_path, proc_path))
+
+script = Popen(['%s/gui/apps.py' %nmrespy_path.rstrip()], shell=True)
