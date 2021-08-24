7fc059dab7ecdfb135854403efc31f9049ee397b
5hulse <simonhulse@protonmail.com>
2021-08-24 14:51:07 +0100

    No longer compatible with Python 3.8

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ setup(
     long_description=long_description,
     long_description_content_type="text/x-rst",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -28,7 +26,7 @@ setup(
         "matplotlib>=3.3",
         "colorama==0.4; platform_system == 'Windows'",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     include_package_data=True,
     packages=find_packages(),
 )
