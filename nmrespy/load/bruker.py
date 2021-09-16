e29ff69a1de23b9b9346f08314642f7638d015e8
5hulse <simonhulse@protonmail.com>
2021-09-16 17:12:08 +0100

    Start to overhaul of loading bruker data.

--- nmrespy/load/bruker.py
+++ nmrespy/load/bruker.py
@@ -1,392 +1,44 @@
-# load.py
-# Simon Hulse
-# simon.hulse@chem.ox.ac.uk
-
 """Provides functionality for importing NMR data."""
 
-from pathlib import Path
 import re
-from typing import FrozenSet, Iterable, List, NewType, Set, Tuple, Union
+from typing import Tuple
 
 import numpy as np
+import bruker_utils
 
 from nmrespy import RED, ORA, END, USE_COLORAMA, ExpInfo
 if USE_COLORAMA:
     import colorama
     colorama.init()
-import nmrespy._errors as errors
 from nmrespy._misc import get_yes_no
 
 
-# Dimension-specifying tags in Bruker parameter files
-# i.e. for a 3D experiment, acqus, acqu2s, and acqu3s files will exist.
-TAGS = ['', '2', '3']
-ListLike = NewType('ListLike', Union[FrozenSet, List, Tuple, Set])
-
-
-def parse_jcampdx(path: Union[Path, str]) -> dict:
-    """Retrieve parameters from files written in JCAMP-DX format.
-
-    Parameters
-    ----------
-    path
-        The path to the parameter file.
-
-    Returns
-    -------
-    A dictionary of parameters. N.B. All returned values are either strings or
-    lists of strings.
-    """
-    with open(path, 'r') as fh:
-        txt = fh.read()
-
-    params = {}
-    array_pattern = r'(?=##\$(.+?)= \(\d+\.\.\d+\)\n([\s\S]+?)##)'
-    array_matches = re.finditer(array_pattern, txt)
-
-    for match in array_matches:
-        key, value = match.groups()
-        params[key] = value.rstrip('\n').replace('\n', ' ').split(' ')
-
-    oneline_pattern = r'(?=##\$(.+?)= (.+?)\n##)'
-    oneline_matches = re.finditer(oneline_pattern, txt)
-
-    for match in oneline_matches:
-        key, value = match.groups()
-        params[key] = value
-
-    return params
-
-
-class BrukerDataset:
-    def __init__(self, dtype: str, dim: int, files: dict) -> None:
-        # Warning text checking that the data being imported has had
-        # `convdta` applied to it.
-        if ask_convdta:
-            convdta_prompt = (
-                f'{ORA}WARNING: It is necessary that the FID you import has '
-                'been digitally filtered, using the <convdta> command in '
-                'TopSpin. If you have not done this, please do so before '
-                'proceeding\nIf you wish to proceed, enter [y]\nIf you wish '
-                f'to quit, enter [n]: {END}'
-            )
-
-        d = Path(directory).resolve()
-        if not d.is_dir():
-            raise IOError(f'\n{RED}Directory {d} doesn\'t exist!{END}')
-
-        try:
-            info = determine_bruker_data_type(d)
-            dim, dtype, files = [info[k] for k in ['data', 'dtype', 'files']]
-            del info
-        # If the directory is invalid, None is returned, which does not have 
-        # a __getitem__ attribute!
-        except TypeError:
-            raise errors.InvalidDirectoryError(d)
-
-        # TODO: 3D data not spported currently. Expect to allow Pseudo-2D data
-        # in the future
-        if info['dim'] > 2:
-            raise errors.MoreThanTwoDimError()
-
-        # If the data to be imported is a raw FID, alert the user about the
-        # necessity to correct for the digital filter
-        if dtype == 'fid' and ask_convdta:
-            get_yes_no(convdta_prompt)
-
-        
-
-
-        self.dim = dim
-        self.dtype = dtype
-        self.datafile = files.pop('data')
-        self.paramfiles = files
-
-    def 
-
-    def get_parameters(
-        self, filenames: Union[ListLike[str], str, None] = None
-    ) -> dict:
-        if isinstance(filenames, str):
-            filenames = [filenames]
-        elif isinstance(filenames, (list, tuple, set, frozenset)):
-            pass
-        elif filenames is None:
-            filenames = [k for k in self.paramfiles.keys()]
-        else:
-            raise TypeError(f'{RED}Invalid type for `filenames`{END}.')
-
-        params = {}
-        for name in filenames:
-            try:
-                params[name] = parse_jcampdx(self.paramfiles[name])
-            except KeyError:
-                raise ValueError(
-                    f'{RED}`{name}` is an invalid filename. Valid options '
-                    f"are:\n{', '.join([k for k in self.paramfiles.keys()])}."
-                    f'{END}'
-                )
-
-        return next(iter(params.values())) if len(params) == 1 else params
-
-    def get_expinfo(self) -> ExpInfo:
-        revtags = reversed(TAGS[:dim])
-        akeys = [f'acqu{x}s' for x in revtags]
-        pts, sw, offset, sfo, nuclei = [[] for  _ in range(5)]
-        for akey in akeys:
-            aparams = self.get_parameters(filenames=akey)
-            sw.append(float(aparams['SW_h']))
-            offset.append(float(aparams['O1']))
-            sfo.append(float(aparams['SFO1']))
-            nuclei.append(aparams['NUC1'])
-
-            if self.dtype == 'fid':
-                td = int(aparams['TD'])
-                if akey == '':
-                    td //= 2
-                pts.append(td)
-            elif self.dtype == 'pdata':
-                pkey = akey.replace('acqu', 'proc')
-                pparams = self.get_parameters(filenames=pkey)
-                pts.append(int(pparams['SI']))
-
-        return ExpInfo(pts, sw, offset, sfo, nuclei)
-
-
-def get_binary_format(info):
-    """Determine the formatting of the binary files.
-
-    Parameters
-    ----------
-    info : dict
-        See :py:func:`determine_bruker_data_type` for a description of items.
-
-    Returns
-    -------
-    binary_format : {'>i4', '<i4', '>f8', '<f8'}
-        The binary file format.
-
-    """
-    if info['dtype'] == 'pdata':
-        names, file = ['DTYPP', 'BYTORDP'], info['param']['procs']
-    else:
-        names, file = ['DTYPA', 'BYTORDA'], info['param']['acqus']
-
-    encoding, endian = [int(p) for p in get_params_from_jcampdx(names, file)]
-
-    return (('<' if endian == 0 else '>') +
-            ('i4' if encoding == 0 else 'f8'))
-
-
-def _determine_data_type(directory: Path) -> Union[BrukerDataset, None]:
-    """Determine the type of Bruker data stored in ``directory``.
-
-    This function is used to determine
-
-    a) whether the specified data is time-domain or pdata
-    b) the dimension of the data (checks up to 3D).
-
-    If the data satisfies the required criteria for a particular dataset type,
-    a dictionary of information will be returned. Otherwise, ``None`` will be
-    returned.
-
-    Parameters
-    ----------
-    directory
-        The path to the directory of interest.
-
-    Returns
-    -------
-    Dictionary with the entries:
-
-    * ``'dim'`` (``int``) The dimension of the data.
-    * ``'dtype'`` (``'fid'`` or ``'pdata'``) The type of data (raw
-      time-domain or pdata).
-    * ``'files'`` (``List[pathlib.Path]``) Paths to data and parameter files.
-    """
-    for option in _compile_experiment_options(directory):
-        files = option['files'].values()
-        if all_paths_exist(files):
-            return BrukerDataset(
-                option['dtype'], option['dim'], option['files']
-            )
-    return None
-
-
-def all_paths_exist(files: Iterable) -> bool:
-    """Determine if all the paths in ``files`` exist.
-
-    Parameters
-    ----------
-    files
-        File paths to check.
-    """
-    return all([f.is_file() for f in files])
-
-
-def _compile_experiment_options(directory: Path) -> List[dict]:
-    """Generate information dictionaries for different experiment types.
-
-    Compiles dictionaries of information relavent to each experiment type:
-
-    * ``'files'`` - The expected paths to data and parameter files.
-    * ``'dim'`` - The data dimension.
-    * ``'dtype'`` - The type of data (time-domain or pdata).
-
-    Parameters
-    ----------
-    directory
-        Path to the directory of interest.
-    """
-    twoback = directory.parents[1]
-    options = []
-    for i in range(1, 4):
-        acqusnames = [f'acqu{x}s' for x in TAGS[:i]]
-        acqusfiles = {
-            name: path for (name, path) in
-            zip(
-                acqusnames,
-                (directory / name for name in acqusnames)
-            )
-        }
-        # for n, p in zip(acqusnames, (directory / nm for nm in acqusnames)):
-        fidfiles = {
-            **{'data': directory / ('fid' if i == 1 else 'ser')},
-            **acqusfiles
-        }
-        options.append(
-            {
-                'files': fidfiles,
-                'dtype': 'fid',
-                'dim': i
-            }
-        )
-
-        acqusfiles = {
-            name: path for (name, path) in
-            zip(
-                acqusnames,
-                (twoback / path.name for path in acqusfiles.values())
-            )
-        }
-        procsnames = [f'proc{x}s' for x in TAGS[:i]]
-        procsfiles = {
-            name: path for (name, path) in
-            zip(
-                procsnames,
-                (directory / name for name in procsnames)
-            )
-        }
-        pdatafiles = {
-            **{'data': directory / f"{i}{i * 'r'}"},
-            **acqusfiles,
-            **procsfiles,
-        }
-
-        options.append(
-            {
-                'files': pdatafiles,
-                'dtype': 'pdata',
-                'dim': i
-            }
-        )
-    print(options)
-    return options
-
-
-def get_parameters(info):
-    """Retrieve various experiment parameters required by NMR-EsPy"""
-
-    shape, nuc, sw, off, sfo, fnmode = [[] for _ in range(6)]
-    sizeparam = 'TD' if info['dtype'] == 'fid' else 'SI'
-
-    for d in reversed(['', '2', '3'][:info['dim']]):
-        acqusfile = info['param'][f'acqu{d}s']
-        if info['dtype'] == 'fid':
-            sizefile = acqusfile
-        else:
-            sizefile = info['param'][f'proc{d}s']
-
-        names = ['NUC1', 'SW_h', 'O1', 'SFO1', 'FnMODE']
-        p = get_params_from_jcampdx(names, acqusfile)
-        p += get_params_from_jcampdx([sizeparam], sizefile)
-        p = iter(p)
-
-        for lst, typ in zip((nuc, sw, off, sfo, fnmode, shape),
-                            (str, float, float, float, int, int)):
-            lst.append(typ(next(p)))
-
-    nuc = [re.search('<(.+?)>', n).group(1) for n in nuc]
-    if sizeparam == 'TD':
-        shape[-1] //= 2
-
-    return {
-        'shape': shape,
-        'nuc': nuc,
-        'sw': sw,
-        'off': off,
-        'sfo': sfo,
-        'fnmode': fnmode,
-    }
-
-
-def import_data(paths, binfmt):
-    return [np.fromfile(file, dtype=binfmt) for file in paths]
-
-
-def reshape_multidim_data(data, shape):
-    """
-    Parameters
-    ----------
-    data : [numpy.ndarray]
-        List of data arrays (initially all flat).
-
-    shape : list
-        Size of each dimension in the data array.
-
-    Returns
-    -------
-    shaped_data : [numpy.ndarray]
-        List of reshaped datasets.
-    """
-    return [d.reshape(shape) for d in data]
-
-
-def complexify(data):
-    """Convert flattened array comprising elements of the form
-    [Re, Im, Re, Im, ...] to a complex array"""
-    return [d[::2] + 1j * d[1::2] for d in data]
-
-
-def remove_zeros(data, shape):
-    """
-    Signals with a TD1 that is not a multiple of 256 are padded with zeros
-    to ensure each FID take up a multiple of 1024 bytes space.
-
-    .. code::
-
-            *******00 *******00 *******00 *******00 *******00
-              FID 1  |  FID 2  |  FID 3  |  FID 4  |  FID 5
-
-    These zeros need to be removed!
-    """
-    # Toal number of datapoints
-    size = data[0].size
-    # Number of FIDs
-    fid_no = np.prod(np.array(shape[:-1]))
-    # Number of points assinged to each FID (inc. zero padding)
-    blocksize = size // fid_no
-    # Number of points in each FID to slice (i.e. the number of zeros)
-    slicesize = blocksize - shape[-1]
-    # Create mask to remove zeros
-    mask = np.ones(size).astype(bool)
-    for n in range(1, fid_no + 1):
-        mask[n * blocksize - slicesize: n * blocksize] = False
-    return [d[mask] for d in data]
-
-
-def load_bruker(directory: str, ask_convdta: bool = True) -> ExpInfo:
-    """Load data and parameters from Bruker format.
+class _BrukerDatasetForNmrespy(bruker_utils.BrukerDataset):
+    def __init__(self, directory: str) -> None:
+        super().__init__(directory)
+
+    @property
+    def expinfo(self) -> ExpInfo:
+        acqusfiles = sorted([f for f in self.valid_parameter_filenames
+                             if 'acqu' in f])
+        sw, offset, sfo, nuclei = [[] for _ in range(4)]
+        for f in acqusfiles:
+            params = self.get_parameters(filenames=f)[f]
+            sw.append(params['SW_h'])
+            offset.append(params['O1'])
+            sfo.append(params['SFO1'])
+            nuclei.append(re.match('^<(.+?)>$', params['NUC1']).group(1))
+
+        pts = self.data.shape
+
+        return ExpInfo(pts=pts, sw=sw, offset=offset, sfo=sfo, nuclei=nuclei,
+                       **{'parameters': self.get_parameters()})
+
+
+def load_bruker(
+    directory: str, ask_convdta: bool = True
+) -> Tuple[np.ndarray, ExpInfo]:
+    """Load data and experiment information from Bruker format.
 
     Parameters
     ----------
@@ -401,25 +53,11 @@ def load_bruker(directory: str, ask_convdta: bool = True) -> ExpInfo:
 
     Returns
     -------
-    result: dict
-        A dictionary containing items with the following keys:
+    data: numpy.ndarray
+        The associated data.
 
-        * `'source'` (str) - The type of data imported (`'bruker_fid'` or
-          `'bruker_pdata'`).
-        * `'data'` (numpy.ndarray) - The data.
-        * `'directory'` (pathlib.Path) - The path to the data directory.
-        * `'sweep_width'` (`[float]` or `[float, float]`) - The experiemnt
-          sweep width in each dimension (Hz).
-        * `'offset'` (`[float]` or `[float, float]`) - The transmitter
-          offset frequency in each dimension (Hz).
-        * `'transmitter_frequency'` (`[float]` or `[float, float]`) - The
-          transmitter frequency in each dimension (MHz).
-        * `'nuclei'` (`[str]` or `[str, str]`) - The nucelus in each
-          dimension.
-        * `'binary_format'` ('str') - The format of the binary data file.
-          Of the form `'<endian><unitsize>'`, where `'<endian>'` is either
-          `'<'` (little endian) or `'>'` (big endian), and `'<unitsize>'`
-          is either `'i4'` (32-bit integer) or `'f8'` (64-bit float).
+    expinfo: nmrespy.ExpInfo
+        Experiment information of use to NMR-EsPy.
 
     Notes
     -----
@@ -500,42 +138,31 @@ def load_bruker(directory: str, ask_convdta: bool = True) -> ExpInfo:
        This would circumvent the need to ask the user to ensure they have
        performed `convdta` prior to importing.
     """
+    try:
+        dataset = bruker_utils.BrukerDataset(directory)
 
-    bruker_dataset = BrukerDataset(dtype, dim, files)
-    expinfo = get_expinfo(all_params)
-    return
+    except Exception as exc:
+        raise exc(f'{RED}{exc.__str__()}{END}')
 
-    get_parameters(info)
-    for k, v in zip(p.keys(), p.values()):
-        info[k] = v
-    info['binfmt'] = get_binary_format(info)
+    if dataset.dtype == 'fid' and ask_convdta:
+        msg = (f'{ORA}You should ensure you data has had its group delay '
+               'artefact removed. Prior to dealing with the data in NMR-EsPy, '
+               'you should call the `CONVDTA` command on the dataset. If this '
+               'has already been done, feel free to proceed. If not, quit the '
+               f'program.\nContinue? [y/n]: {END}')
 
-    data = import_data(paths=info['bin'].values(), binfmt=info['binfmt'])
+        response = get_yes_no(msg)
+        if not response:
+            exit()
+    
+    # Complete set of parameters
+    params = dataset.get_parameters()
+    expinfo = extract_expinfo(dataset)
 
-    if info['dtype'] == 'fid' and info['dim'] > 1:
-        data = \
-            reshape_multidim_data(
-                remove_zeros(
-                    complexify(data), info['shape']
-                ), info['shape']
-            )
 
-    elif info['dtype'] == 'fid':
-        data = complexify(data)
 
-    elif info['dim'] > 1:
-        data = reshape_multidim_data(
-            remove_zeros(data, info['shape']),
-            info['shape']
-        )
 
-    info['data'] = data
-    info['path'] = d
-    info['source'] = 'bruker'
-    del info['bin']
-    del info['param']
 
-    return info
 
 
 # =================================================
--- tests/test_load.py
+++ tests/test_load.py
@@ -2,10 +2,11 @@ from pathlib import Path
 
 import numpy as np
 import pytest
+from bruker_utils import parse_jcampdx
 
+from context import nmrespy
 import nmrespy._errors as err
-from nmrespy.load import bruker as bload
-from nmrespy.load import *
+from nmrespy.load import bruker
 from nmrespy.sig import make_fid
 
 
@@ -14,92 +15,12 @@ PDATAPATHS = [Path(f'data/{i}/pdata/1').resolve() for i in range(1, 3)]
 ALLPATHS = FIDPATHS + PDATAPATHS
 
 
-def test_parse_jcampdx():
-    path = FIDPATHS[0] / 'acqus'
-    params = parse_jcampdx(path)
-    assert params['BYTORDA'] == '0'
-    assert params['CPDPRG'] == 4 * ['<>'] + 5 * ['<mlev>']
-    assert params['SFO1'] == '500.132249206'
-
-
-def test_determine_data_type():
-    for i, (fidp, pdatap) in enumerate(zip(FIDPATHS, PDATAPATHS), start=1):
-        fid_dset = bload._determine_data_type(fidp)
-        pdata_dset = bload._determine_data_type(pdatap)
-
-        assert fid_dset.dim == pdata_dset.dim == i
-
-        assert fid_dset.dtype == 'fid'
-        assert pdata_dset.dtype == 'pdata'
-
-        assert fid_dset.datafile == fidp / ('fid' if i == 1 else 'ser')
-        assert pdata_dset.datafile == pdatap / f"{i}{i * 'r'}"
-
-        for s in ['', '2', '3'][:i]:
-            assert fid_dset.paramfiles[f'acqu{s}s'] == fidp / f'acqu{s}s'
-            assert pdata_dset.paramfiles[f'acqu{s}s'] == \
-                pdatap.parents[1] / f'acqu{s}s'
-            assert pdata_dset.paramfiles[f'proc{s}s'] == pdatap / f'proc{s}s'
-
-
-def test_fetch_parameters():
-    directory = Path(FIDPATHS[0])
-    info = bload._determine_bruker_data_type(directory)
-    params = bload._fetch_parameters(info['param'])
-    print(params)
-
-
-def test_get_expinfo():
-    directory = FIDPATHS[0]
-    parampaths = bload._determine_bruker_data_type(directory)['param']
-    params = bload._fetch_parameters(parampaths)
-    expinfo = bload._get_expinfo(params)
-    print(expinfo)
-
-
-def _funky_mod(x, r):
-    """Same as normal % operator, but if the result is 0, ``r`` is returned
-    instead. i.e. 6 % 3 -> 3 rather than the usual 0."""
-    m = x % r
-    return m if m != 0 else r
-
-
-def test_determine_bruker_data_type():
-    for i, path in enumerate(ALLPATHS, start=1):
-        info = bload.determine_bruker_data_type(path)
-        assert info['dim'] == _funky_mod(i, 2)
-        assert info['dtype'] == 'fid' if i < 3 else 'pdata'
-        assert list(info['param'].keys()) == (
-            [f'acqu{j}s' for j in ['', '2', '3'][:i]]
-            if i < 3
-            else [f'{t}{j}s' for t in ['acqu', 'proc']
-                  for j in ['', '2', '3'][:i - 2]]
-        )
-        if i == 1:
-            assert list(info['bin'].values())[0].name == 'fid'
-        elif i == 2:
-            assert list(info['bin'].values())[0].name == 'ser'
-        else:
-            assert list(info['bin'].values())[0].name == \
-                f"{info['dim']}{info['dim'] * 'r'}"
-
-
-def test_remove_zeros():
-    params = np.array([[1, 0, 2, 3, 0.1, 0.1]])
-    n = [4, 16]
-    sw = [10., 10.]
-    fid = make_fid(params, n, sw)[0].flatten()
-    for zeropad in range(8):
-        shape = [n[0], n[1] - zeropad]
-        for t in range(n[0]):
-            fid[t * n[1] - zeropad: t * n[1]] = 0
-        assert np.any(fid == 0) if zeropad != 0 else True
-        assert fid.size == n[0] * n[1]
-        slyce = bload.remove_zeros([fid], shape)[0]
-        assert slyce.size == n[0] * (n[1] - zeropad)
-        assert np.all(slice != 0)
-
-
-def test_load_bruker():
-    for i, path in enumerate(ALLPATHS):
-        load_bruker(path, ask_convdta=False)
+def test_bruker_dataset_for_nmrespy():
+    dataset = bruker._BrukerDatasetForNmrespy(FIDPATHS[0])
+    expinfo = dataset.expinfo
+    params = parse_jcampdx(FIDPATHS[0] / 'acqus')
+    assert expinfo.sw == (float(params['SW_h']),)
+    assert expinfo.offset == (float(params['O1']),)
+    assert expinfo.sfo == (float(params['SFO1']),)
+    # Strip '<' and '>' characters from ends of string.
+    assert expinfo.nuclei == (params['NUC1'][1:-1],)
