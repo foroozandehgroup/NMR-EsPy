ed19cb3acb0eb11a5184eb2c38d32ec67580cbf7
5hulse <simonhulse@protonmail.com>
2021-09-22 23:33:59 +0100

    Updated csvfile.py. Need to test it!

--- nmrespy/write/csvfile.py
+++ nmrespy/write/csvfile.py
@@ -1,51 +1,48 @@
+import csv
+import datetime
+import pathlib
+from typing import List, Union
+from nmrespy import GRE, END, USE_COLORAMA
+if USE_COLORAMA:
+    import colorama
+    colorama.init()
+
+
 def _write_csv(
-    path, description, info_headings, info, param_titles, param_table,
-    fprint,
-):
+    path: pathlib.Path, param_table: List[List[str]],
+    info_table: List[List[str]], description: Union[str, None], fprint: bool
+) -> None:
     """Writes parameter estimate to a CSV.
 
     Parameters
     -----------
-    path : pathlib.Path
-        File path
+    path
+        File path.
 
-    description : str or None, default: None
+    param_table
+        Table of estimation result parameters.
+
+    info_table
+        Table of experiment information.
+
+    description
         A descriptive statement.
 
-    info_headings : list or None, default: None
-        Headings for experiment information.
-
-    info : list or None, default: None
-        Information that corresponds to each heading in `info_headings`.
-
-    param_titles : list
-        Titles for parameter array table.
-
-    param_table : list
-        Array of contents to append to the result table.
-
-    fprint: bool
+    fprint
         Specifies whether or not to print output to terminal.
     """
-
     with open(path, 'w', encoding='utf-8') as fh:
         writer = csv.writer(fh)
-        # Timestamp
         writer.writerow([_timestamp().replace('\n', ' ')])
         writer.writerow([])
-        # Description
-        if description is not None:
+        if description:
             writer.writerow(['Description:', description])
             writer.writerow([])
-        # Experiment info
-        if info is not None:
-            writer.writerow(['Experiment Info:'])
-            for row in zip(info_headings, info):
-                writer.writerow(row)
-            writer.writerow([])
-        # Parameter table
+        writer.writerow(['Experiment Info:'])
+        for row in info_table:
+            writer.writerow(row)
+        writer.writerow([])
         writer.writerow(['Result:'])
-        writer.writerow(param_titles)
         for row in param_table:
             writer.writerow(row)
 
@@ -53,3 +50,22 @@ def _write_csv(
         print(f'{GRE}Saved result to {path}{END}')
 
 
+def _timestamp() -> str:
+    """Construct a string with time and date information.
+
+    Returns
+    -------
+    timestamp
+        Of the form:
+
+        .. code::
+
+            hh:mm:ss
+            dd-mm-yy
+    """
+    now = datetime.datetime.now()
+    d = now.strftime('%d')  # Day
+    m = now.strftime('%m')  # Month
+    y = now.strftime('%Y')  # Year
+    t = now.strftime('%X')  # Time (hh:mm:ss)
+    return f'{t}\n{d}-{m}-{y}'
