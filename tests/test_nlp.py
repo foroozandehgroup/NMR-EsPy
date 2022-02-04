Fixed any bugs in first and second derivative functions in nlp._funcs.

tests/test_nlp.py
@@ -1,9 +1,9 @@
 # test_nlp.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Tue 01 Feb 2022 12:18:32 GMT
+# Last Edited: Fri 04 Feb 2022 12:00:21 GMT
 
-import copy
+from itertools import combinations
 import numpy as np
 import numpy.linalg as nlinalg
 from nmrespy import ExpInfo, sig
@@ -107,6 +107,10 @@ def test_nlp_2d():
 def test_analytic_grad_hess():
     # Compare analytic and finite difference grad and hessian and check they
     # all closely match.
+
+    h = 0.000001
+
+    # --- 1D ---
     params = np.array([[1.0, 0.0, 5.0, 1.0]])
     x0 = np.array([[0.9, 0.4, 6, 1.2]])
     pts = [4048]
@@ -117,19 +121,58 @@ def test_analytic_grad_hess():
     fid /= norm
     params[0, 0] /= norm
     x0[0, 0] /= norm
-
     x0 = x0.flatten(order='F')
-    params = params.flatten(order='F')
 
-    h = 0.000001
-    args = (fid, tp, x0.size // 4, np.array([]), [0, 1, 2, 3], False)
+    idxs = []
+    for i in range(1, 5):
+        idxs.extend([list(c) for c in combinations(range(4), i)])
 
-    obj_fd, grad_fd, hess_fd = _funcs.obj_finite_diff_grad_hess_1d(x0, h, *args)
-    obj_ex, grad_ex, hess_ex = _funcs.obj_grad_true_hess_1d(x0, *args)
+    for idx in idxs:
+        active = x0[idx]
+        passive = x0[[i for i in range(4) if i not in idx]]
+        args = (fid, tp, 1, passive, idx, False)
 
-    for fd, ex in zip(grad_fd, grad_ex):
-        assert np.allclose(fd, ex, rtol=0, atol=0.01)
+        obj_fd, grad_fd, hess_fd = _funcs.obj_finite_diff_grad_hess_1d(active, h, *args)
+        obj_ex, grad_ex, hess_ex = _funcs.obj_grad_true_hess_1d(active, *args)
 
-    for row_fd, row_ex in zip(hess_fd, hess_ex):
-        for fd, ex in zip(row_fd, row_ex):
-            assert np.allclose(fd, ex, rtol=0, atol=0.01)
+        assert np.allclose(grad_fd, grad_ex, rtol=0, atol=0.01)
+        assert np.allclose(hess_fd, hess_ex, rtol=0, atol=0.01)
+
+    # --- 2D ---
+    params = np.array([[1.0, 0.0, 5.0, -7.0, 2.0, 1.0]])
+    x0 = np.array([[0.9, 0.4, 6, -8.0, 1.8, 1.2]])
+    pts = [128, 128]
+    expinfo = ExpInfo(sw=100, dim=2)
+
+    fid, tp = sig.make_fid(params, expinfo, pts)
+    norm = nlinalg.norm(fid)
+    fid /= norm
+    params[0, 0] /= norm
+    x0[0, 0] /= norm
+    x0 = x0.flatten(order='F')
+
+    idxs = []
+    for i in range(1, 7):
+        idxs.extend([list(c) for c in combinations(range(6), i)])
+
+    for i, idx in enumerate(idxs):
+        # Filter away combinations that are not feasible.
+        # 2 and 3 will always exist together (f1 and f2).
+        # 4 and 5 will always exist together (η1 and η2).
+        if not (
+            (2 in idx and 3 not in idx) or
+            (3 in idx and 2 not in idx) or
+            (4 in idx and 5 not in idx) or
+            (5 in idx and 4 not in idx)
+        ):
+            active = x0[idx]
+            passive = x0[[i for i in range(6) if i not in idx]]
+            args = (fid, tp, 1, passive, idx, False)
+
+            obj_fd, grad_fd, hess_fd = _funcs.obj_finite_diff_grad_hess_2d(
+                active, h, *args,
+            )
+            obj_ex, grad_ex, hess_ex = _funcs.obj_grad_true_hess_2d(active, *args)
+
+            assert np.allclose(grad_fd, grad_ex, rtol=0, atol=0.02)
+            assert np.allclose(hess_fd, hess_ex, rtol=0, atol=0.02)