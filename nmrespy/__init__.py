5f0ee8c98828fbe1d1d4756fadabc14874e00670
5hulse <simonhulse@protonmail.com>
2022-05-03 16:28:33 +0100

    Try to fix publish-docs #4

--- nmrespy/__init__.py
+++ nmrespy/__init__.py
@@ -1,10 +1,11 @@
 # __init__.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Tue 03 May 2022 16:19:38 BST
+# Last Edited: Tue 03 May 2022 16:28:10 BST
 
 """NMR-EsPy: Nuclear Magnetic Resonance Estimation in Python."""
 
-from nmrespy._version import *  # noqa F403
+import nmrespy._version as nv
+__version__ = nv.__version__
 from nmrespy.expinfo import ExpInfo
 from nmrespy.estimators.onedim import Estimator1D
--- nmrespy/_version.py
+++ nmrespy/_version.py
@@ -1,6 +1,6 @@
 # _version.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Tue 03 May 2022 16:16:57 BST
+# Last Edited: Tue 03 May 2022 16:26:44 BST
 
 __version__ = "1.0"
