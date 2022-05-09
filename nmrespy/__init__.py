53da2357250703a24f2922e5e20b56c04c1b83d9
5hulse <simonhulse@protonmail.com>
2022-05-09 07:53:37 +0100

    Bump to 1.1

--- nmrespy/__init__.py
+++ nmrespy/__init__.py
@@ -1,10 +1,11 @@
 # __init__.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Mon 09 May 2022 07:41:37 BST
+# Last Edited: Mon 09 May 2022 07:52:38 BST
 
 """NMR-EsPy: Nuclear Magnetic Resonance Estimation in Python."""
 
-__version__ = "1.1"
+import nmrespy._version as nv
+__version__ = nv.__version__
 from nmrespy.expinfo import ExpInfo
 from nmrespy.estimators.onedim import Estimator1D
